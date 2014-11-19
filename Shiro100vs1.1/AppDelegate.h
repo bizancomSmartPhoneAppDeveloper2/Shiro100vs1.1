8fe73335dfe0adae83210f092b56d02a0af76323
寺内 信夫 <tenobu@gmail.com>
2014-11-19 15:26:07 +0900

    Initial Commit

---
+++ Shiro100vs1.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shiro100vs1.1
+//
+//  Created by 寺内 信夫 on 2014/11/19.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
